2a4fad499169b342d76b8564bd18de47c32b3639
wealcodersk <jahid.wealcoder@gmail.com>
2023-08-06 18:21:46 +0600

    first commit

---
+++ app/users/page.tsx
@@ -0,0 +1,13 @@
+import { Metadata } from "next"
+
+export const metadata : Metadata = {
+  title: 'Users',
+}
+
+export default function UserPage() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
