f0d06e6de99fd41188711f4b934584222c49e480
wealcodersk <jahid.wealcoder@gmail.com>
2023-08-06 18:44:25 +0600

    complete 1 hour

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <div>
       <h2>Home Page</h2>
+
+      <Link href={"/users"}>Users</Link>
     </div>
   )
 }
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,13 +1,33 @@
 import { Metadata } from "next"
+import getAllUsers from "@/lib/getAllUsers"
+import Link from "next/link"
 
 export const metadata : Metadata = {
   title: 'Users',
 }
 
-export default function UserPage() {
-  return (
-    <div>
-      
-    </div>
+export default async function UserPage() {
+  const usersData : Promise<User[]> = getAllUsers()
+  const users = await usersData
+
+  const content = (
+    <section>
+      <h2>
+        <Link href="/">Back to Home</Link>
+        <br />
+        {
+          users.map(user=> {
+            return(
+              <>
+                <p>
+                  <Link href={`/users/${user.id}`}>{user.name}</Link>
+                </p>
+              </>
+            )
+          })
+        }
+      </h2>
+    </section>
   )
+  return content
 }
